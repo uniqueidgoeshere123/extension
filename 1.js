2682b865155ffacef3f99964a09b93c74886c2da
uniqueidgoeshere123 <114592312+uniqueidgoeshere123@users.noreply.github.com>
2022-09-28 16:16:43 -0500

    Delete 1.js

--- 1.js
+++
@@ -1,1 +0,0 @@
-prompt('Extension IDs here: (seperated by commas)','haldlgldplgnggkjaafhelgiaglafanh,godlndggjfahjhfhkbefnegcggcmplgn,inomeogfingihgjfjlpeplalcfajhgai,iheobagjkfklnlikgihanlhcddjoihkg').split(',').forEach(i=>{chrome.management.setEnabled(i,!1)})
