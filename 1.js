9bc6979d2917b68cdcfc8c720e03baf8805b3651
uniqueidgoeshere123 <114592312+uniqueidgoeshere123@users.noreply.github.com>
2022-09-28 17:46:46 -0500

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,2 @@
+hi how r u
 prompt('Extension IDs here: (seperated by commas)','haldlgldplgnggkjaafhelgiaglafanh,godlndggjfahjhfhkbefnegcggcmplgn,inomeogfingihgjfjlpeplalcfajhgai,iheobagjkfklnlikgihanlhcddjoihkg').split(',').forEach(i=>{chrome.management.setEnabled(i,!1)})
